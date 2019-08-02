e902bd28183db40c9aafd5f8c6ffd6c4b6c5f875
Dariusz Różecki <dariuszrozecki@macbook-pro-dariusz.local>
2019-08-02 20:02:57 +0200

    Initial Commit

---
+++ TODO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TODO
+//
+//  Created by Dariusz Różecki on 02/08/2019.
+//  Copyright © 2019 Dariusz Różecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
