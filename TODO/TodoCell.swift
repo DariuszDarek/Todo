f70dd3bc1e797aa446f5af7cff66914ce054fd3f
Dariusz Różecki <dariuszrozecki@MacBook-Pro-Dariusz.local>
2019-08-05 14:10:49 +0200

    my project

---
+++ TODO/TodoCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoCell.swift
+//  TODO
+//
+//  Created by Dariusz Różecki on 02/08/2019.
+//  Copyright © 2019 Dariusz Różecki. All rights reserved.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
